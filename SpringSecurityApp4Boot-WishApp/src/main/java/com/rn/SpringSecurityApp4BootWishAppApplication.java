b10f86bc83f129678b35396fb183c5d4d2226080
Roshan Nemad <nemadroshan@gmail.com>
2020-05-28 15:37:11 +0530

    commited SpringSecurtt+SpringBoot App

---
+++ SpringSecurityApp4Boot-WishApp/src/main/java/com/rn/SpringSecurityApp4BootWishAppApplication.java
@@ -0,0 +1,13 @@
+package com.rn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApp4BootWishAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApp4BootWishAppApplication.class, args);
+	}
+
+}
---
+++ SpringSecurityApp4Boot-WishApp/src/test/java/com/rn/SpringSecurityApp4BootWishAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApp4BootWishAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
